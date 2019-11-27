3e2839422820bfb83cfa5594db915869b692c594
947133713@qq.com <947133713@qq.com>
2019-11-27 18:20:40 +0800

    fix

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,3 +1,3 @@
 <?php
 Route::any('/gii/model','\Sunshinev\Gii\Controllers\ModelController@index');
-Route::any('/gii/crud','\Sunshinev\Gii\Controllers\CrudController@index')
+Route::any('/gii/crud','\Sunshinev\Gii\Controllers\CrudController@index');
